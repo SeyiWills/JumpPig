ccf236c47db970c153218cfb4e3432845ce37c3b
Ramiro Restrepo <ramres111@yahoo.com>
2016-04-28 09:54:53 -0400

    our  code for our game

--- JumpPig/GameScene.swift
+++ JumpPig/GameScene.swift
@@ -9,37 +9,131 @@
 import SpriteKit
 
 class GameScene: SKScene {
-    override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 45;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
+    
+    // Tap To Start node
+    let tapToStartNode = SKSpriteNode(imageNamed: "TapToStart")
+    
+    
+    // Layered Nodes
+    var backgroundNode: SKNode!
+    var midgroundNode: SKNode!
+    var foregroundNode: SKNode!
+    var hudNode: SKNode!
+    
+    // To Accommodate iPhone 6
+    var scaleFactor: CGFloat!
+    var player: SKNode!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
     }
     
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
+    override init(size: CGSize) {
+        super.init(size: size)
+        backgroundColor = SKColor.whiteColor()
         
-        for touch in touches {
-            let location = touch.locationInNode(self)
+        
+        // Add some gravity
+        physicsWorld.gravity = CGVector(dx: 0.0, dy: -2.0)
+        
+        scaleFactor = self.size.width / 320.0
+        
+        // Create the game nodes
+        // Background
+        backgroundNode = createBackgroundNode()
+        addChild(backgroundNode)
+        
+        // Foreground
+        foregroundNode = SKNode()
+        addChild(foregroundNode)
+        
+        // HUD
+        hudNode = SKNode()
+        addChild(hudNode)
+        
+        // Add the player
+        player = createPlayer()
+        foregroundNode.addChild(player)
+        
+        
+    }
+    
+
+    
+    
+    
+    
+    
+    func createBackgroundNode() -> SKNode {
+        // 1
+        // Create the node
+        let backgroundNode = SKNode()
+        let ySpacing = 64.0 * scaleFactor
+        
+        // 2
+        // Go through images until the entire background is built
+        for index in 0...0 {
+            // 3
+            let node = SKSpriteNode(imageNamed:String(format: "Background", index + 1))
+            // 4
+            node.setScale(scaleFactor)
+            node.anchorPoint = CGPoint(x: 0.5, y: 0.0)
+            node.position = CGPoint(x: self.size.width / 2, y: ySpacing * CGFloat(index))
+            //5
+            backgroundNode.addChild(node)
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
             
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
             
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
         }
+        
+        // 6
+        // Return the completed background node
+        return backgroundNode
     }
-   
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
+    
+    
+    func createPlayer() -> SKNode {
+        let playerNode = SKNode()
+        playerNode.position = CGPoint(x: self.size.width / 2, y: 80.0)
+        
+        let sprite = SKSpriteNode(imageNamed: "Pig")
+        playerNode.addChild(sprite)
+        
+        // Tap to Start
+        tapToStartNode.position = CGPoint(x: self.size.width / 2, y: 180.0)
+        hudNode.addChild(tapToStartNode)
+        
+        
+        // 1
+        playerNode.physicsBody = SKPhysicsBody(circleOfRadius: sprite.size.width / 2)
+        // 2
+        playerNode.physicsBody?.dynamic = false
+        // 3
+        playerNode.physicsBody?.allowsRotation = false
+        // 4
+        playerNode.physicsBody?.restitution = 1.0
+        playerNode.physicsBody?.friction = 0.0
+        playerNode.physicsBody?.angularDamping = 0.0
+        playerNode.physicsBody?.linearDamping = 0.0
+        
+        
+        
+        
+        
+        return playerNode
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
+
+
+
+
